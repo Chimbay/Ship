c32fd27983b0ec933b46ac1307c427e299239b03
Chimbay <c.anthony.priv@gmail.com>
2024-03-22 23:43:41 -0400

    Adding and enabled screen wrapping for the ship

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -9,7 +9,9 @@ public class Ship : MonoBehaviour
     // Direction of thrust
     Vector2 thrustDirction;
     // Setting thrust force
-    const float thrustForce = 20;
+    const float thrustForce = 300;
+    // Step 4 - Storing ship collider
+    private CircleCollider2D cc2d;
 
     // Start is called before the first frame update
     void Start()
@@ -18,12 +20,13 @@ public class Ship : MonoBehaviour
         rb2d = GetComponent<Rigidbody2D>();
         // Initializing direction of thrust
         thrustDirction = new Vector2(1, 0);
+        // Initializing ship collider
+        cc2d = GetComponent<CircleCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     // Unlike update, FixedUpdate() updates physics in a consistent manner (like Time.
@@ -34,6 +37,39 @@ public class Ship : MonoBehaviour
         {
             Debug.Log("space key was pressed");
             rb2d.AddForce(thrustDirction * thrustForce, ForceMode2D.Force);
+        }
+    }
+
+    // OnBecameInvisible is called when the renderer is no longer visible by any camera.
+    void OnBecameInvisible()
+    {
+        Vector3 position = transform.position;
+        if (position.x - cc2d.radius < ScreenUtils.ScreenLeft)
+        {
+            // Screen wrapping to the left
+            position.x = ScreenUtils.ScreenRight + cc2d.radius;
+            transform.position = position;
+        }
+
+        if (position.y + cc2d.radius > ScreenUtils.ScreenTop)
+        {
+            // Screen wrapping from the top
+            position.y = ScreenUtils.ScreenBottom - cc2d.radius;
+            transform.position = position;
+        }
+
+        if (position.x + cc2d.radius > ScreenUtils.ScreenRight)
+        {
+            // Screen wrapping to the right;
+            position.x = ScreenUtils.ScreenLeft - cc2d.radius;
+            transform.position = position;
+        }
+
+        if (position.y - cc2d.radius < ScreenUtils.ScreenBottom)
+        {
+            // Screen wrapping from the bottom
+            position.y = ScreenUtils.ScreenTop + cc2d.radius;
+            transform.position = position;
         }
     }
 }
